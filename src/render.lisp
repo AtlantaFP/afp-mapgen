149313c6856f639f9c7d2c4b725c83557b8a7b26
Elijah Malaby <qwe12345678910@gmail.com>
2018-08-13 11:46:48 -0400

    Added pixmap rendering

--- src/render.lisp
+++ src/render.lisp
@@ -61,24 +61,19 @@
   (setf *stage* (apply #'make-stage attrs))
   (make-instance 'afp-mapgen :attrs attrs))
 
-(clim:define-application-frame mcclim-mapgen-renderer ()
-  ((attrs :initarg :attrs :reader attrs)
-   (stage :reader stage))
-  (:panes (toplevel :application-pane :display-function 'render-with-mcclim
-                                      :background (apply #'clim:make-rgb-color
-                                                         *wall-color*)
-                                      :width 1 :height 1))
-  (:layouts (default toplevel)))
+(defclass stage-display-pane (clim:basic-gadget)
+  ((pixmap :reader pixmap :initform nil)))
 
-(defmethod initialize-instance :after ((renderer mcclim-mapgen-renderer) &key attrs)
-  (setf (slot-value renderer 'stage) (apply #'make-stage attrs)))
-
-(defun render-with-mcclim (frame pane)
-  (let* ((stage (stage frame))
-         (opts (options stage)))
-    (clim:with-output-recording-options (pane :record t :draw t)
-      (loop :for x :below (width opts) :do
-        (loop :for y :below (height opts) :do
+(defun draw-stage-to-pixmap (stage pane)
+  (let* ((opts (options stage))
+         (width (width opts))
+         (height (height opts)))
+    (clim:with-output-to-pixmap (pixmap pane :width (* width *cell-size*)
+                                             :height (* height *cell-size*))
+      (clim:draw-rectangle* pixmap 0 0 (* width *cell-size*) (* height *cell-size*)
+                            :ink (apply #'clim:make-rgb-color *wall-color*))
+      (loop :for x :below width :do
+        (loop :for y :below height :do
           (when (feature-present-p (get-cell stage x y) :room)
             (clim:draw-rectangle* pane
                                   (* x *cell-size*) (* y *cell-size*)
@@ -86,13 +81,42 @@
                                   :ink (apply #'clim:make-rgb-color
                                               (select-color :rect (get-cell stage x y))))))))))
 
+(defmethod clim:handle-repaint ((pane stage-display-pane) region)
+  (declare (ignore region))
+  (with-slots (pixmap) pane
+    (unless pixmap
+      (setf (slot-value pane 'pixmap)
+            (draw-stage-to-pixmap (stage clim:*application-frame*) pane))
+      (clim:change-space-requirements pane :width (clim:pixmap-width pixmap)
+                                           :height (clim:pixmap-height pixmap)))
+    (clim:copy-from-pixmap pixmap
+                           0 0 (clim:pixmap-width pixmap) (clim:pixmap-height pixmap)
+                           pane 0 0)))
+
+(clim:define-application-frame mcclim-mapgen-renderer ()
+  ((attrs :initarg :attrs :reader attrs)
+   (stage :reader stage))
+  (:panes (toplevel (clim:make-pane 'stage-display-pane)))
+  (:layouts (default toplevel)))
+
+
+(defmethod initialize-instance :after ((renderer mcclim-mapgen-renderer) &key attrs)
+  (let ((stage (apply #'make-stage attrs)))
+    (setf (slot-value renderer 'stage) stage)))
+
 (define-mcclim-mapgen-renderer-command (com-redisplay :name t :menu t)
     ())
 
 (define-mcclim-mapgen-renderer-command (com-new-map :name t :menu t)
     ()
-  (setf (slot-value clim:*application-frame* 'stage)
-        (apply #'make-stage (afp-utils:plist-remove (attrs clim:*application-frame*) :seed))))
+  (let ((stage (apply #'make-stage (afp-utils:plist-remove (attrs clim:*application-frame*)
+                                                           :seed))))
+    (let ((p (clim:find-pane-named clim:*application-frame* 'toplevel)))
+      (when (pixmap p)
+        (clim:deallocate-pixmap (pixmap p)))
+      (setf (slot-value clim:*application-frame* 'stage) stage
+            (slot-value p 'pixmap) nil)
+      (clim:handle-repaint p clim:+everywhere+))))
 
 
 (defmethod render ((method (eql :mcclim)) &rest attrs)
@@ -101,6 +125,8 @@
                                                                      :attrs attrs)))
     (labels ((run ()
                (unwind-protect (clim:run-frame-top-level frame)
+                 (when (pixmap (clim:find-pane-named frame 'toplevel))
+                   (clim:deallocate-pixmap (pixmap (clim:find-pane-named frame 'toplevel))))
                  (clim:disown-frame fm frame))))
       (clim-sys:make-process #'run))))
 
