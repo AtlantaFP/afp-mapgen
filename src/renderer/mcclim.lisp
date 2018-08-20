19285ba498168934dbb415f86df54dcbfb9a6a10
Elijah Malaby <qwe12345678910@gmail.com>
2018-08-20 13:40:10 -0400

    Tweaked the McClim renderer for better responsiveness

--- src/renderer/mcclim.lisp
+++ src/renderer/mcclim.lisp
@@ -3,6 +3,7 @@
 (defclass stage-display-pane (clim:basic-gadget)
   ((pixmap :accessor pixmap :initform nil)
    (cell-size :accessor cell-size :initform *cell-size* :initarg :cell-size)
+   (draw-connectors-p :accessor draw-connectors-p :initform nil)
    (stage :accessor stage :initarg :stage :initform nil))
   (:default-initargs :background (apply #'clim:make-rgb-color *wall-color*)))
 
@@ -19,7 +20,7 @@
   (setf (slot-value pane 'pixmap) nil)
   (clim:handle-repaint pane clim:+everywhere+))
 
-(defun draw-stage (stage width height medium cell-size)
+(defun draw-stage (stage width height medium &key (cell-size *cell-size*) (draw-connectors-p t))
   (clim:draw-rectangle* medium 0 0 (* width cell-size) (* height cell-size)
                         :ink (apply #'clim:make-rgb-color *wall-color*))
   (loop :for x :below width :do
@@ -29,7 +30,7 @@
                               (* x cell-size) (* y cell-size)
                               (- (* (1+ x) cell-size) 1) (- (* (1+ y) cell-size) 1)
                               :ink (apply #'clim:make-rgb-color (select-color :rect (get-cell stage x y)))))
-      (when (feature-present-p (get-cell stage x y) :connector)
+      (when (and draw-connectors-p (feature-present-p (get-cell stage x y) :connector))
         (clim:draw-circle* medium
                            (+ (* x cell-size) (floor cell-size 2))
                            (+ (* y cell-size) (floor cell-size 2))
@@ -42,7 +43,7 @@
          (height (height opts)))
     (clim:with-output-to-pixmap (pixmap pane :width (* width cell-size)
                                              :height (* height cell-size))
-      (draw-stage stage width height pixmap cell-size))))
+      (draw-stage stage width height pixmap :cell-size cell-size :draw-connectors-p (draw-connectors-p pane)))))
 
 (defmethod clim:handle-repaint ((pane stage-display-pane) region)
   (declare (ignore region))
@@ -58,7 +59,7 @@
 
 (clim:define-application-frame mcclim-mapgen-renderer ()
   ((attrs :initarg :attrs :accessor attrs))
-  (:panes (toplevel (clim:make-pane 'stage-display-pane :cell-size *cell-size*))
+  (:panes (toplevel (clim:make-pane 'stage-display-pane :cell-size *cell-size* :draw-connectors-p t))
           (stage-width :text-editor :value (write-to-string (getf (attrs clim:*application-frame*) :width 49)))
           (stage-height :text-editor :value (write-to-string (getf (attrs clim:*application-frame*) :height 49)))
           (stage-seed :text-editor :value (write-to-string (getf (attrs clim:*application-frame*) :seed 1)))
@@ -88,64 +89,77 @@
                         :activate-callback
                         (lambda (button)
                           (declare (ignore button))
-                          (let* ((attrs (attrs clim:*application-frame*))
-                                 (stage (apply #'make-stage attrs))
-                                 (top (clim:find-pane-named clim:*application-frame* 'toplevel)))
-                            (setf (stage top) stage)
-                            (redraw-pixmap top))))
+                          (render-map-from-attrs)))
+          (draw-connectors-checkbox :toggle-button
+                                    :indicator-type :some-of
+                                    :value-changed-callback 'new-draw-connectors-setting)
           (cell-width-slider :slider :value *cell-size*
                                      :orientation :horizontal
                                      :show-value-p t
                                      :value-changed-callback 'new-cell-size
                                      :min-value 5 :max-value 20))
-  (:layouts (default (clim:horizontally ()
+  (:layouts (default (clim:horizontally (:equalize-height t)
                        (1/2 (clim:vertically ()
                               (clim:scrolling () toplevel)
                               (clim:horizontally ()
-                                (1/16 (clim:make-pane :label :label "Cell Size:"))
-                                (1/32 (clim:make-pane :label :label "5px"))
-                                (7/8 cell-width-slider)
-                                (1/32 (clim:make-pane :label :label "20px")))))
+                                (clim:vertically ()
+                                  (clim-label "Cell Size:")
+                                  (clim:horizontally ()
+                                    (1/8 (clim-label "5px"))
+                                    (3/4 cell-width-slider)
+                                    (1/8 (clim-label "20px"))))
+                                (clim:vertically (:align-x :center)
+                                  (clim-label "Draw Connectors")
+                                  draw-connectors-checkbox))))
                        (1/2 (clim:vertically ()
-                              (clim:horizontally ()
-                                (clim-label "Stage width (odd integer): ")
-                                stage-width)
-                              (clim:horizontally ()
-                                (clim-label "Stage height (odd integer): ")
-                                stage-height)
-                              (clim:horizontally ()
-                                (clim-label "Stage room extent (integer): ")
-                                stage-room-extent)
-                              (clim:horizontally ()
-                                (clim-label "Stage room density (between 0.1 and 1.0): ")
-                                stage-density)
-                              (clim:horizontally ()
-                                (clim-label "Stage cycle factor (between 0.0 and 1.0): ")
-                                stage-cycle-factor)
-                              (clim:horizontally ()
-                                (clim-label "Stage door rate (between 0.0 and 1.0): ")
-                                stage-door-rate)
-                              (clim:horizontally ()
-                                (clim-label "Stage meander rate (between 0.0 and 1.0): ")
-                                stage-wild-factor)
-                              (clim:horizontally ()
-                                (clim-label "Stage seed (integer): ")
-                                stage-seed
-                                (clim:make-pane :push-button
-                                                :label "New Seed"
-                                                :activate-callback
-                                                (lambda (button)
-                                                  (declare (ignore button))
-                                                  (setf (clim:gadget-value stage-seed :invoke-callback nil)
-                                                        (write-to-string (make-seed))))))
+                              (clim:tabling ()
+                                (list
+                                 (clim-label "Stage room density (between 0.1 and 1.0): ")
+                                 stage-density)
+                                (list
+                                 (clim-label "Stage cycle factor (between 0.0 and 1.0): ")
+                                 stage-cycle-factor)
+                                (list
+                                 (clim-label "Stage door rate (between 0.0 and 1.0): ")
+                                 stage-door-rate)
+                                (list
+                                 (clim-label "Stage meander rate (between 0.0 and 1.0): ")
+                                 stage-wild-factor)
+                                (list
+                                 (clim-label "Stage width (odd integer): ")
+                                 stage-width)
+                                (list
+                                 (clim-label "Stage height (odd integer): ")
+                                 stage-height)
+                                (list
+                                 (clim-label "Stage room extent (integer): ")
+                                 stage-room-extent)
+                                (list
+                                 (clim-label "Stage seed (integer): ")
+                                 (clim:horizontally ()
+                                   stage-seed
+                                   (clim:make-pane :push-button
+                                                   :label "New Seed"
+                                                   :activate-callback
+                                                   (lambda (button)
+                                                     (declare (ignore button))
+                                                     (setf (clim:gadget-value stage-seed :invoke-callback nil)
+                                                           (write-to-string (make-seed))))))))
                               apply-button))))))
+
+(defun render-map-from-attrs ()
+  (let* ((attrs (attrs clim:*application-frame*))
+         (stage (apply #'make-stage attrs))
+         (top (clim:find-pane-named clim:*application-frame* 'toplevel)))
+    (setf (stage top) stage)
+    (redraw-pixmap top)))
 
 (defun try-parse-integer (string)
   (multiple-value-bind (num idx) (parse-integer string :junk-allowed t)
     (values (or num 0) (= idx (length string)))))
 
 (defmethod clim:value-changed-callback :after (gadget (frame mcclim-mapgen-renderer) id value)
-  (declare (ignore gadget id value))
+  (declare (ignore id value))
   (unless (eq :disowned (clim:frame-state frame))
     (afp-utils:mvlet* ((width parsed-full-width
                               (try-parse-integer (clim:gadget-value (clim:find-pane-named frame 'stage-width))))
@@ -173,13 +187,21 @@
       (if valid-p
           (progn
             (clim:activate-gadget (clim:find-pane-named frame 'apply-button))
-            (setf (attrs frame) attrs))
+            (setf (attrs frame) attrs)
+            (when (typep gadget 'clim:slider-pane)
+              (render-map-from-attrs)))
           (clim:deactivate-gadget (clim:find-pane-named frame 'apply-button))))))
 
 (defun new-cell-size (slider value)
   (declare (ignore slider))
   (let ((top (clim:find-pane-named clim:*application-frame* 'toplevel)))
     (setf (cell-size top) value)
+    (redraw-pixmap top)))
+
+(defun new-draw-connectors-setting (checkbox value)
+  (declare (ignore checkbox))
+  (let ((top (clim:find-pane-named clim:*application-frame* 'toplevel)))
+    (setf (draw-connectors-p top) value)
     (redraw-pixmap top)))
 
 (defun next-lowest-odd (num)
@@ -191,20 +213,9 @@
     (setf (stage top) stage)))
 
 (defmethod clim:run-frame-top-level :before ((renderer mcclim-mapgen-renderer) &key)
+  (setf (clim:gadget-value (clim:find-pane-named renderer 'stage-seed) :invoke-callback t)
+        (write-to-string (getf (attrs renderer) :seed 1)))
   (update-stage renderer))
-
-(define-mcclim-mapgen-renderer-command
-    (com-new-map :name t :menu t)
-    ()
-  (let ((stage (apply #'make-stage (afp-utils:plist-remove (attrs clim:*application-frame*)
-                                                           :seed)))
-        (top (clim:find-pane-named clim:*application-frame* 'toplevel)))
-    (setf (stage top) stage)
-    (redraw-pixmap top)))
-
-(define-mcclim-mapgen-renderer-command (com-redraw-pixmap :name t :menu t)
-    ()
-  (redraw-pixmap (clim:find-pane-named clim:*application-frame* 'toplevel)))
 
 (defun clim-label (text)
   (clim:make-pane :label :label text))
